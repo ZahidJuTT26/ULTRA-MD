9be3a1316b68848b55c894cda3fb13c5fa6a3b15
Muhmmad ⃢ZāHīd <99365528+ZahidJuTT26@users.noreply.github.com>
2024-12-22 17:48:56 +0500

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ for (let i = 0; i < ownerlist.length; i++) {
 //
 global.botname = process.env.BOTNAME || 'ULTRA-MD';
 global.pairingNumber = process.env.BOT_NUMBER || '923276326230';  // put your number here
-global.SESSION_ID = process.env.SESSION_ID || 'session id';  // put your session id here
+global.SESSION_ID = process.env.SESSION_ID || 'bAp3XSjQ#suVIcMVq1p9baEp6Nx92LYT6EU4-MASSfuQGBn7_uKQ';  // put your session id here
 
 global.mods = []
 global.prems = []
